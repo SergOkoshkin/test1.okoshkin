c6aeae0a2c1effe271940d2dc28a197014b5a679
SergOkoshkin <tvbox.marchserg@gmail.com>
2026-04-09 13:20:23 +0300

    Rename next.config.ts to next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Твои настройки (если есть)
+  images: {
+    domains: [], // если используешь внешние изображения
+  },
+  // другие опции...
+}
+
+module.exports = nextConfig
